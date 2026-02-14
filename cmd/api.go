cdc3eb821183fef12959a353b9ecfa9b1f821000
Tobi-20 <olutobiseun18@gmail.com>
2026-02-14 22:52:15 +0100

    first commit

---
+++ cmd/api.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type application struct {
+	config config
+	db     *pgx.Conn
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+	return srv.ListenAndServe()
+}
